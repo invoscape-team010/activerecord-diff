17b926c36079af94bc089d5ffb0e4f17fc7ebd56
invoscape-team010 <team010@invoscape.com>
2014-07-01 12:36:32 +0530

    Skipping attrs that can be ignored

--- lib/active_record/diff.rb
+++ lib/active_record/diff.rb
@@ -24,9 +24,7 @@ module ActiveRecord
 
       if new_record.is_a?(Hash)
         diff_each(new_record) do |(attr_name, hash_value)|
-          puts attr_name.to_sym
-          puts attrs_to_be_deleted.include?(attr_name.to_sym)
-          next if attrs_to_be_deleted.include?(attr_name)
+          next if attrs_to_be_deleted.include?(attr_name.to_sym)
           [attr_name, old_record.send(attr_name), hash_value]
         end
       else
